bdd810328754f8992c7ccfa8c857f8b19b1bac66
Pieter Beerten <pieter.beerten00@gmail.com>
2022-12-10 21:25:15 +0100

    adapted influence of polarization mismatch, relative angle and two-ray model

--- Propagation_Models.m
+++ Propagation_Models.m
@@ -7,7 +7,7 @@ addpath('Data','Functions','Images')
 load('./Data/Center_Frequency.mat')
 
 D = 16.9*10^-2; % [m]
-lambda_B = 3*10^8/freq_center_A; % [m]
+lambda_B = 3*10^8/freq_center; % [m]
 [R_Fresnel_B, R_Fraunhofer_B] = calculateRegions(D,lambda_B); % [m]
 
 d = [3.81	 3.23    2.63    2.03    1.43    0.83]; % [m]
@@ -53,16 +53,15 @@ close all
     
 theta = 0:15:90; % [°]
 P = [-36.2  -37.2   -39.5   -46.7   -54.4   -62.4   -52.3]; % [dBm]
-attenuation = 10 - P; % transmit 10 dBm
+%attenuation = 10 - P; % transmit 10 dBm
 
 figure();
-%polarplot( deg2rad(theta) ,attenuation,'*--')
 polarplot( deg2rad(theta) ,P,'*--')
 title('Polarization mismatch')
 thetalim([theta(1) theta(end)]);
 rlim([-65 -35])
-legend('Received Power')
-exportgraphics(gcf,'./Images/Polarization.png')
+
+exportgraphics(gcf,'./Images/FreeSpace_Polarization.png')
 
     %% Relative angle
 close all
@@ -70,39 +69,59 @@ close all
 theta = -90:15:90; % [°]
 P = [-45.1  -45.0   -55.8   -51.7   -44.3   -40.9   -36.8 ...
      -35.1  -36.5   -38.0   -43.9   -54.8   -46.2]; % [dBm]
-attenuation = 10 - P; % transmit 10 dBm
+%attenuation = 10 - P; % transmit 10 dBm
 
 figure();
-polarplot( deg2rad(theta) ,attenuation)
+polarplot( deg2rad(theta) ,P,'*--')
+title('Relative angle')
 thetalim([theta(1) theta(end)]);
+rlim([-59 -35])
+
+pax = gca;
+pax.ThetaDir = 'counterclockwise';
+pax.ThetaZeroLocation = 'top';
 
 exportgraphics(gcf,'./Images/FreeSpace_Relative_Angle.png')
 
 %% Two-ray model
-
+clc, close all
     %% position of hole
-
+close all
 
 P = [-37.9  -37.7   -37.8   -38.1   -37.9   -37.6];
-attenuation = 10 - P; % transmit 10 dBm
+%attenuation = 10 - P; % transmit 10 dBm
 
 figure();
-plot(attenuation)
-
+plot(P,'*--')
+grid on, grid minor
+xlabel('Position of hole')
+ylabel('Received power [dBm]')
+title('Influence of hole position')
 exportgraphics(gcf,'./Images/TwoRay_Position.png')
+
+P_sort = sort(P);
+rel_diff = abs((P_sort(1) - P_sort(end)))/abs(P_sort(1));
+fprintf('The relative difference between the highest and lowest received power is %0.2f%%. \n', rel_diff*100)
+
 
     %% number of holes
 
 n_holes = 0:10;
 P = [-37.9	-37.6   -37.8   -38.1   -38.3   -38.7 ...
      -38.2  -38.6   -38.6   -38.4   -38.3];
-attenuation = 10 - P; % transmit 10 dBm
+%attenuation = 10 - P; % transmit 10 dBm
 
 figure();
-plot(n_holes, attenuation)
-
+plot(n_holes, P, '*--')
+grid on, grid minor
+xlabel('Number of holes')
+ylabel('Received power [dBm]')
+title('Influence of number of holes')
 exportgraphics(gcf,'./Images/TwoRay_NumberHoles.png')
 
+P_sort = sort(P);
+rel_diff = abs((P_sort(1) - P_sort(end)))/abs(P_sort(1));
+fprintf('The relative difference between the highest and lowest received power is %0.2f%%. \n', rel_diff*100)
 
 %% Small-scale fading
     %% Extract the data
